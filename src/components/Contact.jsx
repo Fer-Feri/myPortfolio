fbe099c4d9934d92c1e41672a1dbb0474b2e9b18
aloy33 <fsrshad.bahari.33@gmail.com>
2025-02-08 11:44:36 +0330

    rewrite conttact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,17 +1,12 @@
-/* eslint-disable react/no-unknown-property */
-
 import { Canvas, useFrame } from "@react-three/fiber";
-import { useRef, useLayoutEffect, useState } from "react";
+import { useRef, useLayoutEffect, useState, useMemo } from "react";
 import { useGLTF, Stage } from "@react-three/drei";
-
-import { motion, useScroll, useTransform } from "framer-motion";
+import { motion, useScroll } from "framer-motion";
 
 // وارد کردن مدل‌های سه‌بعدی
 import teleIcon3D from "../assets/3d/tele.glb";
 import instaIcon3D from "../assets/3d/insta.glb";
 import inIcon3D from "../assets/3d/in.glb";
-// import whatsIcon3D from "../assets/3d/whats.glb";
-// import gitIcon3D from "../assets/3d/git.glb";
 import gitIcon3D from "../assets/3d/git.glb";
 
 //---------------------
@@ -44,7 +39,7 @@ const StaticModel = ({
 }) => {
   const meshRef2 = useRef();
   const { scene } = useGLTF(modelPath);
-  // تغییر رنگ مدل
+
   useLayoutEffect(() => {
     if (scene) {
       scene.traverse((child) => {
@@ -82,9 +77,12 @@ const ContactMe = () => {
     [0, -3, 0],
   ]);
 
-  const models = [gitIcon3D, instaIcon3D, inIcon3D, teleIcon3D];
-  const rotationSpeeds = [0.5, 0.7, 0.9, 0.6]; // سرعت چرخش برای هر آیکون
-  const colors = [null, "#C13584", null, null]; // رنگ‌های مختلف برای هر مدل
+  const models = useMemo(
+    () => [gitIcon3D, instaIcon3D, inIcon3D, teleIcon3D],
+    [],
+  );
+  const rotationSpeeds = [0.5, 0.7, 0.9, 0.6];
+  const colors = [null, "#C13584", null, null];
   const links = [
     "https://github.com/Fer-Feri",
     "https://www.instagram.com/far._.shad_/",
@@ -120,19 +118,13 @@ const ContactMe = () => {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  // ---------------------------
   const { scrollYProgress } = useScroll();
-  // const scaleText = useTransform(scrollYProgress, [0.1, 0.9], [0.9, 1]);
 
   return (
     <div className="bg-red h-screen w-full" id="contact">
-      {/* //!title */}
       <div className="mb-[20rem] mt-20 max-lg:mb-[22rem] max-md:mb-[1rem]">
         <div className="relative flex justify-center py-24 font-poppinsB max-2xl:py-14 max-lg:py-16 max-md:py-12 max-sm:py-12 max-[400px]:py-8">
-          <p
-            // style={{ scale: scaleText }}
-            className="strokeText absolute top-[85%] text-[13rem] text-transparent dark:stroke-mainYellow max-md:top-[92%] max-md:text-[9rem] max-sm:top-[95%] max-sm:text-[7rem]"
-          >
+          <p className="strokeText absolute top-[85%] text-[13rem] text-transparent dark:stroke-mainYellow max-md:top-[92%] max-md:text-[9rem] max-sm:top-[95%] max-sm:text-[7rem]">
             Contact
           </p>
           <h2 className="absolute top-full text-[11rem] text-neutral-900 dark:text-mainYellow max-md:text-[8rem] max-sm:text-[6rem]">
@@ -150,13 +142,13 @@ const ContactMe = () => {
               position={pos}
               size={size}
               modelPath={models[index]}
-              rotationSpeed={rotationSpeeds[index]} // تنظیم سرعت چرخش
-              color={colors[index]} // ارسال رنگ به مدل
+              rotationSpeed={rotationSpeeds[index]}
+              color={colors[index]}
               scale={
                 index === 0
                   ? [size / 2, size / 2, size / 2]
                   : [size / 3, size / 3, size / 3]
-              } // تغییر مقیاس
+              }
               onClick={() => window.open(links[index], "_blank")}
             />
           ))}
